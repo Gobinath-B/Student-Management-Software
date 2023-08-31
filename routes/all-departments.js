b74f4d6d3394194d50a630e9e2bfc7b4bd0e3ee1
iwint <iwint.cseasnsct@gmail.com>
2023-09-01 04:25:19 +0530

    Student CRUD Integration - Needs to be done

--- routes/all-departments.js
+++ routes/all-departments.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const fb = require('../config/fb')
-const db = fb.firestore()
+/** @format */
 
-router.get('/',async (req,res)=>{
-    const response = await db.collection('college_info').doc('cIajardAYY7ySgxH3nY1').get()
-    const get_data = response.data()
-    res.render('all-departments',{data:get_data.department, id:response.id})
-})
+const express = require("express");
+const router = express.Router();
+const fb = require("../config/fb");
+const db = fb.firestore();
 
-module.exports = router
+router.get("/", async (req, res) => {
+     const response = await db.collection("college_info").doc("cIajardAYY7ySgxH3nY1").get();
+     const get_data = response.data();
+     res.render("all-departments", { data: get_data.department, id: response.id });
+});
+
+module.exports = router;
--- routes/edit-student.js
+++ routes/edit-student.js
@@ -1,7 +1,40 @@
+/** @format */
 
-const express = require('express')
-const router = express.Router()
-router.get('/',(req,res)=>{
-    res.render('edit-student')
-})
-module.exports = router
+const express = require("express");
+const router = express.Router();
+const fb = require("../config/fb");
+const db = fb.firestore();
+router.get("/:id", async (req, res) => {
+     const id = req.params.id;
+     const depart_id = req.query.id;
+     const response_data = await db.collection("students").doc(id).get();
+     const actual_data = response_data.data();
+     console.log("ACTUAL JOB", actual_data);
+     //  res.render("edit-student", {
+     //       data: actual_data.department[depart_id],
+     //  });
+});
+router.post("/:id", async (req, res) => {
+     const id = req.params.id;
+     const depart_id = req.query.id;
+     console.log(depart_id);
+     const update_department_value = req.body.department;
+     const response_data = await db.collection("college_info").doc(id).get();
+     const result_data = response_data.data().department;
+     result_data[`${depart_id}`] = update_department_value;
+     console.log(result_data);
+     await db.collection("college_info").doc(id).update({ department: result_data });
+     res.redirect("/all-departments");
+});
+router.get("/delete/:id", async (req, res) => {
+     const id = req.params.id;
+     const depart_id = req.query.id;
+     console.log(depart_id);
+     const response_data = await db.collection("college_info").doc(id).get();
+     var result_data = response_data.data().department;
+     delete result_data[`${depart_id}`];
+     console.log(result_data);
+     await db.collection("college_info").doc(id).update({ department: result_data });
+     res.redirect("/all-departments");
+});
+module.exports = router;
