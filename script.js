db07375db4fbbfb842d8cb78e815c1a1308292ea
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-08-31 17:25:35 +0530

    feat: app middle ware

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 const fs = require('fs')
 
 const middle_route = (endpoint)=>`
-const express = require('express')
-const router = express.Router()
-router.get('/',(req,res)=>{
-
-})
-module.exports = router
-`
+app.use('/${endpoint}',${endpoint.replaceAll('-','_')})`
 
 const files = fs.readdirSync(__dirname+"/views")
 files.forEach(file=>{
     if(file.includes('.ejs')){
         let oldFileName = file.split('.')[0];
-        fs.writeFileSync(__dirname+"/routes/"+oldFileName+'.js',middle_route(oldFileName))
+        fs.appendFileSync("temp.js",middle_route(oldFileName))
     }
 })
