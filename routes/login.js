27f469ea005de24794c91df82f189233f6de9cea
iwint <iwint.cseasnsct@gmail.com>
2023-08-31 23:50:01 +0530

    1.Login page implemented 2.Footer changes done 3.Conflicts b/w new features resolved

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express')
-const router = express.Router()
-const auth = require('./../middleware/auth')
-router.get('/',(req,res)=>{
-    res.render('login')
-})
-router.post('/',auth.login)
-module.exports = router
