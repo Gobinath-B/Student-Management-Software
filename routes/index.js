0c3c684720077eb8248ee1fbcd5410764cbb6bd6
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-08-31 22:32:31 +0530

    feat: sns label added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
-
+var cookie = require('cookie');
+const fb = require('./../config/fb').firestore()
 const express = require('express')
 const router = express.Router()
-router.get('/',(req,res)=>{
-    res.render('index')
+
+
+router.get('/',async (req,res)=>{
+    var cookies = cookie.parse(req?.headers?.cookie || '');
+    const id = cookies?.student_id
+    const student_response = await (await fb.collection('students').doc(id).get()).data()
+    console.log(student_response);
+    res.render('index',{data:student_response})
 })
 module.exports = router
