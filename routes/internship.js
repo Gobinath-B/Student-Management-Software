e738e8fc5cfc7ec0b4c56bf87de834446c3a0d54
Gobinath <princegopinath42@gmail.com>
2023-08-31 09:36:11 +0530

    routes

---
+++ routes/internship.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+const StudentModel = require('./models/schema.js');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/",(req,res)=>{res.render('index')});
+
+module.exports = internship;
