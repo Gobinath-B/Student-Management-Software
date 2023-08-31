b34755efa85478dffbfa792ab509418736eb5f7b
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-08-31 20:43:23 +0530

    feat: style format

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,24 +6,23 @@ var cookie = require('cookie');
 exports.login = async (req, res, next) =>{
     try{
         const {user_name,  password, type} = req.body
-
         if(type == "student"){
-                const response = await db.collection('students')
-                .where('email',"==",user_name)
-                .where('reg_id',"==",password)
-                .get()
-                const result = []
-                await response.docs.forEach(std=>{
-                    result.push({id:std.id,...std.data()})
-                })
-                console.log(result);
-                if(result.length==0){
-                    res.send('User Not found')
-                    return;
-                }else{
-                    res.setHeader('Set-Cookie',cookie.serialize('student_id',String(result[0].id)))
-                    res.redirect('/')
-                }
+            const response = await db.collection('students')
+            .where('email',"==",user_name)
+            .where('reg_id',"==",password)
+            .get()
+            const result = []
+            await response.docs.forEach(std=>{
+                result.push({id:std.id,...std.data()})
+            })
+            console.log(result);
+            if(result.length==0){
+                res.send('User Not found')
+                return;
+            }else{
+                res.setHeader('Set-Cookie',cookie.serialize('student_id',String(result[0].id)))
+                res.redirect('/')
+            }
         }else{
 
         }
