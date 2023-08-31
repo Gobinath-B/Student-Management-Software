11c974a2c24622ea29edd484b61eb82f64e8b218
Gobinath <princegopinath42@gmail.com>
2023-09-01 04:35:48 +0530

    feat: fees-module

--- routes/fees-collection.js
+++ routes/fees-collection.js
@@ -1,7 +1,16 @@
 
 const express = require('express')
 const router = express.Router()
+const fb = require('../config/fb');
+const db = fb.firestore();
+var cookie = require("cookie");
+
 router.get('/',(req,res)=>{
-    res.render('fees-collection')
+    const reqdata = req.body;
+    var cookies = cookie.parse(req?.headers?.cookie || "");
+    const id = cookie.student_id;
+    const result = db.collection("students").doc(id).get();
+    const data = result.data();
+    res.render('fees-collection',{data:data});
 })
 module.exports = router
