280cbbcfab9e5bfbb1ac0245155a6a4fe7ba4584
Gratus Richard <gratusrichardsocial@gmail.com>
2023-09-01 06:36:07 +0530

    sfgs

--- routes/edit-library.js
+++ routes/edit-library.js
@@ -2,6 +2,6 @@
 const express = require('express')
 const router = express.Router()
 router.get('/',(req,res)=>{
-    res.render('edit-library')
+    res.render('edit-library',{careerRecommendation:null})
 })
 module.exports = router
